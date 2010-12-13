7319b7d38f505a2df7246b6e1f5e2a5f659f533b
Alexander Korsak <alex.korsak@gmail.com>
2010-12-13 12:31:08 +0200

    Implement rotations for elements

--- scripts/Shape.js
+++ scripts/Shape.js
@@ -2,6 +2,7 @@ Shape = Klass(CanvasNode, {
 	map: [],
 	drag: false,
 	degree: 0,
+	current_map: [],
 	
 	initialize: function(options) {
 		CanvasNode.initialize.call(this);
@@ -16,25 +17,45 @@ Shape = Klass(CanvasNode, {
 			if (options.cy != null) {
 				this.cy = options.y;
 			}
+			if (options.degree != null) {
+				this.degree = options.degree
+			}	
 		}
+		
+		this.current_map = this.map[this.degree];
 		
 		this.addEventListener('drag', function(e) {
 			this.x += (this.x - e.x > 0 ? -1 : 1);
 			this.y += (this.y - e.y > 0 ? -1 : 1);
 		});
 
-		this.addEventListener('')
+		this.addEventListener('click', function() {
+			this.rotate();
+		});
 		
-		this.render_shape();
+		this.addFrameListener(function() {
+			this.render_shape();
+		});
 	},
 
 	rotate: function() {
+		this.degree = this.ensure_degree(this.degree + 1);
+		this.current_map = this.map[this.degree];
+		this.removeAllChildren();
+	},
+	
+	ensure_degree: function(degree) {
+		if (degree >= this.map.length) {
+			degree = 0;
+		}
+		return degree;
 	},
 	
 	render_shape: function() {
-		for(var i = 0; i < this.map.length; i++) {
-			for(var j = 0; j < this.map[i].length; j++) {
-				if (this.map[i][j] == 1) {
+		for(var i = 0; i < this.current_map.length; i++) {
+			for(var j = 0; j < this.current_map[i].length; j++) {
+				if (this.current_map[i][j] == 1) {
+					
 					this.append(new Block({cx: this.x + j * 10, cy: this.y + i * 10}));
 				}
 			}
