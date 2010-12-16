da873372251c70546cb036321a38be9bc52a5942
Alexander Korsak <alex.korsak@gmail.com>
2010-12-17 00:39:10 +0200

    replace tabs with spaces

--- scripts/Block.js
+++ scripts/Block.js
@@ -1,21 +1,21 @@
 Block = Klass(CanvasNode, {
-	backgroud: 'rgb(0, 0, 0)',
-	opacity: 0.8,
-	size: 20,
-	
-	initialize: function(options) {
-		CanvasNode.initialize.call(this);
-		
-		this.shape = new Rectangle(this.size, this.size, {
-			fill: this.background,
-			stroke: 'cyan', 
-			strokeWidth: 1,
-			x: options.x,
-			y: options.y,
-			rx: 2,
-			ry: 2
-		});
-		
-		this.append(this.shape);
-	}	
+  backgroud: 'rgb(0, 0, 0)',
+  opacity: 0.8,
+  size: 20,
+  
+  initialize: function(options) {
+    CanvasNode.initialize.call(this);
+    
+    this.shape = new Rectangle(this.size, this.size, {
+      fill: this.background,
+      stroke: 'cyan', 
+      strokeWidth: 1,
+      x: options.x,
+      y: options.y,
+      rx: 2,
+      ry: 2
+    });
+    
+    this.append(this.shape);
+  }  
 });
--- scripts/GameContainer.js
+++ scripts/GameContainer.js
@@ -1,54 +1,54 @@
 GameContainer = Klass(CanvasNode, { 
-	score: new ScorePanel(),
-	background: '#FFFFFF',
-	width: 300,
-	height: 480,
-	
-	initialize: function(canvasElem) {
-		CanvasNode.initialize.call(this);
-		
-		this.canvas = new Canvas(canvasElem);
-		this.canvas.frameDuration = 30;
-		this.canvas.append(this);
-		
-		// Draw container where we will place shape objects.
-		this.container = new Rectangle(this.width, this.height, {
-		  fill: this.background,
-		  rx: 2,
-		  ry: 2,
-		  strokeWidth: 1,
-		  stroke: 'cyan',
-		  x: 0,
-		  y: 0
-		});
-		
-		this.append(this.container);
+  score: new ScorePanel(),
+  background: '#FFFFFF',
+  width: 300,
+  height: 480,
+  
+  initialize: function(canvasElem) {
+    CanvasNode.initialize.call(this);
+    
+    this.canvas = new Canvas(canvasElem);
+    this.canvas.frameDuration = 30;
+    this.canvas.append(this);
+    
+    // Draw container where we will place shape objects.
+    this.container = new Rectangle(this.width, this.height, {
+      fill: this.background,
+      rx: 2,
+      ry: 2,
+      strokeWidth: 1,
+      stroke: 'cyan',
+      x: 0,
+      y: 0
+    });
+    
+    this.append(this.container);
 
-		// This panel will generate next shape and preview it.
-		// Score panel also contains preview for the game object
-		// and scores what use have got.
-		this.append(this.score);
-		for(var i = 0; i < 1; i++) {
-			this.append(this.score.next());
-		}
-		
-	},
+    // This panel will generate next shape and preview it.
+    // Score panel also contains preview for the game object
+    // and scores what use have got.
+    this.append(this.score);
+    for(var i = 0; i < 1; i++) {
+      this.append(this.score.next());
+    }
+    
+  },
 
-	start: function() {
-		this.addFrameListener(function(){
-				
-		});	
-	},
+  start: function() {
+    this.addFrameListener(function(){
+        
+    });  
+  },
 
-	pause: function() {
-		
-	},
+  pause: function() {
+    
+  },
 
-	restart: function() {
-	
-	},
+  restart: function() {
+  
+  },
 
-	stop: function() {
-		  
-	}
+  stop: function() {
+      
+  }
 });
