ba3f849ced1ac29598cf24f3172ccf5bd4e54491
Alexandr Korsak <alex.korsak@gmail.com>
2012-09-07 18:33:27 +0300

    Set content type for the manifest

--- server.rb
+++ server.rb
@@ -7,6 +7,12 @@ get '/' do
   send_file File.join(settings.public_folder, 'game.html')
 end
 
+get '/manifest' do
+  content_type 'application/x-web-app-manifest+json'
+
+  send_file File.join(settings.public_folder, 'manifest.txt')
+end
+
 get '/about' do
   'author: alex.korsak@gmail.com'
 end
